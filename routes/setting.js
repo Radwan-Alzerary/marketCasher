60192e47e3a8ea67a003400e858e2f2857dfe539
radwan <rdoaniv@ymail.com>
2024-08-27 21:22:35 +0300

    v0.234

--- routes/setting.js
+++ routes/setting.js
@@ -8,6 +8,7 @@ const Delevery = require("../models/delevery");
 const isfulladmin = require("../config/auth").isfulladmin;
 const isCashire = require("../config/auth").isCashire;
 const ensureAuthenticated = require("../config/auth").userlogin;
+const mongoose = require("mongoose");
 
 // const Sitting = require("../models/pagesitting");
 const multer = require("multer");
@@ -81,5 +82,68 @@ router.get("/getsetting", async (req, res) => {
   const setting = await Setting.findOne();
   res.json(setting);
 });
+
+
+
+router.get("/exportdata", async (req, res) => {
+  try {
+    const modelNames = mongoose.modelNames();
+
+    // Ensure the data directory exists
+    const dataDirectory = "../backup";
+    if (!fs.existsSync(dataDirectory)) {
+      fs.mkdirSync(dataDirectory);
+    }
+
+    const exportPromises = modelNames.map(async (modelName) => {
+      const Model = mongoose.model(modelName);
+      const data = await Model.find({});
+      const dataPath = `../backup/${modelName}.json`;
+
+      // Write the data to the file
+      fs.writeFileSync(dataPath, JSON.stringify(data));
+
+      console.log(`Exported ${modelName} to ${dataPath}`);
+    });
+
+    await Promise.all(exportPromises);
+    await SystemSetting.findOneAndUpdate({}, { lastBackup: Date.now() });
+
+    res.status(200).json({ message: "All models exported successfully" });
+  } catch (error) {
+    console.error("Error exporting models", error);
+    res.status(500).json({ error: "Error exporting models", details: error });
+  }
+});
+
+router.get("/importdata", async (req, res) => {
+  try {
+    const modelNames = mongoose.modelNames();
+
+    for (const modelName of modelNames) {
+      const dataPath = `../backup/${modelName}.json`;
+
+      // Read the JSON data from the file
+      const rawData = fs.readFileSync(dataPath, "utf8");
+      const data = JSON.parse(rawData);
+
+      const Model = mongoose.model(modelName);
+
+      // Clear existing data in the model (optional, depending on your needs)
+      await Model.deleteMany({});
+
+      // Insert the data into the model
+      await Model.insertMany(data);
+
+      console.log(`Imported data for ${modelName}`);
+    }
+
+    res.status(200).json({ message: "Data imported successfully" });
+  } catch (error) {
+    console.error("Error importing data", error);
+    res.status(500).json({ error: "Error importing data", details: error });
+  }
+});
+
 
 module.exports = router;
